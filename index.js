ea31826a256821881e904156a1f4d1d67b00810e
Alex Schin <schinalex@gmail.com>
2020-10-03 22:25:36 +0300

    get tickets

--- index.js
+++ index.js
@@ -37,6 +37,27 @@ const App = () => (
   </div>
 )
 
+const searchUrl = 'https://front-test.beta.aviasales.ru/tickets?searchId=3a40w'
+
+const get = url => new Promise((resolve, reject) => {
+  const http = new XMLHttpRequest()
+  http.open('GET', url)
+  http.send()
+  http.onreadystatechange = () => {
+    if (http.readyState === 4) {
+      if (http.status === 200) {
+        resolve(http.response)
+      } else {
+        reject(http.response, http.status)
+      }
+    }
+  }
+})
+
+get(searchUrl).then((response) => {
+  console.log(JSON.parse(response))
+})
+
 ReactDOM.render(e(App), document.querySelector('#app'))
 
 console.log('It kinda works')
